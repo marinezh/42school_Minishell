a8ab6a9b9cd4725c81d8e889d9b308ddfe58f71c
Marina Zhivotova <mazhivotova@gmail.com>
2025-04-02 18:35:02 +0300

    add readline

--- src/main.c
+++ src/main.c
@@ -6,7 +6,25 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 13:10:49 by mzhivoto          #+#    #+#             */
-/*   Updated: 2025/03/28 13:11:01 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2025/04/02 18:34:05 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+char *readline (const char *prompt)
+{
+
+}
+
+
+int main(int ac, char **av)
+{
+    char *prompt = av[1];
+    char *line = readline(prompt);
+
+    printf("readline: %s\n", line);
+    printf("prompt is: %s\n", prompt);
+    return 0;
+
+}
