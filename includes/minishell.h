efe8a81e485495739f2328992dbd53f5016eee4d
Marina Zhivotova <mazhivotova@gmail.com>
2025-04-03 19:19:20 +0300

    fix readline, now it works

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 13:20:43 by mzhivoto          #+#    #+#             */
-/*   Updated: 2025/03/30 17:29:17 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2025/04/03 17:06:52 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,21 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
 
+# define RESET		"\033[0m"
+# define BLACK		"\033[30m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+# define WHITE		"\033[37m"
+# define BOLD		"\033[1m"
+#define PROMPT	"\001" GREEN BOLD "\002minishell$ \001" RESET "\002"
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,25 +6,34 @@
 /*   By: mzhivoto <mzhivoto@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 13:10:49 by mzhivoto          #+#    #+#             */
-/*   Updated: 2025/04/02 18:34:05 by mzhivoto         ###   ########.fr       */
+/*   Updated: 2025/04/03 18:02:19 by mzhivoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char *readline (const char *prompt)
-{
 
+int main(int ac, char **av, char **env)
+{
+	char *input;
+
+	(void)av;
+	while (1)
+	{
+		input = readline("minishell$ ");
+
+	 
+		if (!input)
+			continue;
+		
+		if (*input)
+		{
+			add_history(input);
+			printf("%s\n", input);
+		}
+		free(input);
+	}
+	return 0;
 }
 
-
-int main(int ac, char **av)
-{
-    char *prompt = av[1];
-    char *line = readline(prompt);
-
-    printf("readline: %s\n", line);
-    printf("prompt is: %s\n", prompt);
-    return 0;
-
-}
+// if input exists check it for errros and tokenize it with linked list
