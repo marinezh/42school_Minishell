4826dabb928d211b4f78f2541cdb667df211c4d3
Marina Zhivotova <mazhivotova@gmail.com>
2025-04-27 17:55:09 +0300

    work on structs content

--- includes/structs.h
+++ includes/structs.h
@@ -10,6 +10,14 @@ typedef enum e_token_type
 	REDIR_APPEND,
 	HEREDOC
 }						t_token_type;
+typedef enum e_redirection
+{
+	STD, // standart in/out
+	REDIR_IN, 
+	REDIR_OUT,
+	REDIR_APPEND,
+	HEREDOC
+}						t_redirection;
 
 typedef struct s_token
 {
@@ -18,15 +26,44 @@ typedef struct s_token
 	struct s_token		*next;
 }						t_token;
 
+// typedef struct s_command
+// {
+// 	char **args;            // command + arguments
+// 	char *infile;           // for <
+// 	char *outfile;          // for > or >>
+// 	int append;             // 1 if >>, 0 if >
+// 	int pipe;               // 1 if followed by |
+// 	struct s_command *next; // next command in pipeline
+// }						t_command;
+
+// linked list of structs split by pipe
+typedef struct s_files
+{
+	int	fd;
+	char	*name;
+	t_redirection 	red_types;  //  < << >> > 
+} t_files;
+
 typedef struct s_command
 {
-	char				**args;			// command + arguments
-	char				*infile;		// for <
-	char				*outfile;		// for > or >>
-	int					append;			// 1 if >>, 0 if >
-	int					pipe;			// 1 if followed by |
-	struct s_command	*next;			// next command in pipeline
-}			t_command;
+	int					index;
+	char **args;		// command + arguments
+	char *infile;		// for < or << (herdoc)
+	char *outfile;		// for > or >>
+	int append;			// 1 if >>, 0 if >
+	int pipe;			// 1 if followed by |
+	t_token				*tokens;
+	t_files				**redirections;
+	struct s_command	*next;
+}						t_command;
+
+// // pattens of logical command split by pipe connected in linke list
+// typedef struct s_cmd_node
+// {
+// 	int					key;
+// 	char				*value;
+// 	struct s_cmd_node	*next;
+// }						t_cmd_node;
 
 typedef struct s_cmd_input
 {
@@ -58,26 +95,9 @@ struct					s_data
 	t_bltin				builtins[7];
 	char				**envp;
 	t_env				*envp_list;
-	char        envp_f;
+	char				envp_f;
 	// char        exit_t;
 	// int         status;
 };
-
-// pattens of logical command split by pipe connected in linke list
-typedef	struct s_cmd_node
-{
-	int	key;
-	char *value;
-	struct s_cmd_node	*next;
-} t_cmd_node;
-
-// linked list of structs split by pipe
-typedef	struct s_command
-{
-	int index;
-	char	**value;
-	t_cmd_node *cmd_nodes;
-	struct s_command *next;
-} t_command;
 
 #endif
