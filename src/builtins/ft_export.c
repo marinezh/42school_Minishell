65fec2bd09dde381c7b0aedd8986bd7c03aef4d4
Inna Kozhina <inna.kozhina22@gmail.com>
2025-05-07 13:20:42 +0300

    variables name check when come with export command, check for flag with export

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -60,6 +60,50 @@ void	create_env_var(t_data *data, char *arg)
 	}
 }
 
+void	print_error_msg(char **args, char *msg)
+{
+	int i;
+
+	printf("usage: ");
+	i = 0;
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	printf("%s", msg);
+}
+
+int	is_var_name_valid(char *arg, char **args)
+{
+	int	i;
+
+	i = 0;
+	if (!((arg[0] >= 'a' && arg[0] <= 'z') ||
+		(arg[0] >= 'A' && arg[0] <= 'Z') ||
+		arg[0] == '_'))
+	{
+		print_error_msg(args, ": variable name is invalid\n");
+		return (0);
+	}
+	i = 1;
+	while (arg[i] && arg[i] != '=')
+	{
+		if (!((arg[i] >= 'a' && arg[i] <= 'z') ||
+			(arg[i] >= 'A' && arg[i] <= 'Z') ||
+			(arg[i] >= '0' && arg[i] <= '9') ||
+			arg[i] == '_'))
+		{
+			print_error_msg(args, ": variable name is invalid\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	ft_export(t_data *data, t_command *cmd)
 {
 	int		arg_count;
@@ -77,10 +121,17 @@ int	ft_export(t_data *data, t_command *cmd)
 	{
 		while (i < arg_count)
 		{
-			cur = data->envp_list;
+			if (cmd->args[i][0] == '-')
+			{
+				print_error_msg(cmd->args, ": command with option is invalid\n");
+				break ;
+			}
+			if (!is_var_name_valid(cmd->args[i], cmd->args))
+				break ;
 			printf("arg 2 - %s\n", cmd->args[i]);
 			name_len = var_name_len(cmd->args[i]);
 			found = 0;
+			cur = data->envp_list;
 			printf("name len - %d\n", name_len);
 			while (cur != NULL)
 			{
@@ -105,5 +156,6 @@ int	ft_export(t_data *data, t_command *cmd)
 //export - prints variables in alphabetic order!
 //export -p - should program notify about not-handling flags!
 
-//1 - validate variables names(start and follow) before adding to linked list
+//1 - validate variables names created with export
+// check -(start and follow) before adding to linked list
 //2 - edge cases (e.g. export =value, ""=value, export+=value)
