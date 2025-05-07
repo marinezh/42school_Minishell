e7c9da5aeeff113345e477ab9019c21aede930ac
Inna Kozhina <inna.kozhina22@gmail.com>
2025-05-07 11:49:00 +0300

    export print format in alphabetic order

--- src/builtins/export_print.c
+++ src/builtins/export_print.c
@@ -1,32 +1,72 @@
 #include "minishell.h"
 
+void sort_envp_by_key(char **envp)
+{
+	int		i;
+	int		swap;
+	char	*tmp;
+
+	swap = 1;
+	while (swap)
+	{
+		swap = 0;
+		i = 0;
+		while (envp[i])
+		{
+			if (envp[i + 1] && ft_strcmp(envp[i], envp[i + 1]) > 0)
+			{
+				tmp = envp[i];
+				envp[i] = envp[i + 1];
+				envp[i + 1] = tmp;
+				swap = 1;
+			}
+			i++;
+		}
+	}
+}
+
+void	print_export_format(char **envp)
+{
+	int		i;
+	char	*ptr;
+	char	*value;
+	int		key_len;
+
+	i = 0;
+	while (envp[i])
+	{
+		ptr = ft_strchr(envp[i], '=');
+		if (ptr)
+		{
+			key_len = ptr - envp[i];
+			value = ptr + 1;
+		}
+		else
+		{
+			key_len = (int)ft_strlen(envp[i]);
+			value = NULL;
+		}
+		if (value)
+			printf("declare -x %.*s=\"%s\"\n", key_len, envp[i], value);
+		else
+			printf("declare -x %.*s\n", key_len, envp[i]);
+		i++;
+	}
+}
+
 void	print_current_envp(t_data *data, t_command *cmd)
 {
-	// int i;
-
-	// i = 0;
-	// (void)cmd;
-	// if (data->envp_f)
-	// {
-	// 	update_envp_array(data, data->envp_list);
-	// 	data->envp_f = 0;
-	// }
-	// if (data->envp)
-	// {
-	// 	while (data->envp[i])
-	// 	{
-	// 		printf("declare -x %s\n", data->envp[i]);
-	// 		i++;
-	// 	}
-	// }
-	size_t	list_len;
-	t_env	**arr_nodes;
-
-	list_len = env_list_size(data->envp_list);
-	arr_nodes = malloc(sizeof(t_env *) * list_len);
-	if (!arr_nodes)
-		return ;
-
-
-
+	(void)cmd;
+	if (data->envp_f)
+	{
+		update_envp_array(data, data->envp_list);
+		data->envp_f = 0;
+	}
+	if (data->envp)
+	{
+		sort_envp_by_key(data->envp);
+		print_export_format(data->envp);
+		free_envp_array(data->envp);
+		data->envp = NULL;
+	}
 }
