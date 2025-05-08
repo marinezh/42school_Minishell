29fced8ac9aeba0055ff7c371f36340e2dd14e58
KozhInna <inna.kozhina22@gmail.com>
2025-05-08 15:41:46 +0300

    add enum for error codes, ft_env add check for options or arguments

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -2,10 +2,15 @@
 
 int ft_env(t_data *data, t_command *cmd)
 {
-	(void)cmd;
 	int	i;
 
 	i = 0;
+    if (cmd->args[1])
+    {
+        ft_putstr_fd("minishell: env: ", STDERR_FILENO);
+        ft_putstr_fd("options or arguments are not supported\n", STDERR_FILENO);
+        return (ERROR_GENERIC);
+    }
 	if (data->envp_f)
 	{
 		update_envp_array(data, data->envp_list);
@@ -23,5 +28,5 @@ int ft_env(t_data *data, t_command *cmd)
 	return(0);
 }
 //print only variables with value, don't print empty variables
-//if anything is typied after env - no other parameters are allowed after env only |
+//if anything is typied after env - no other parameters are allowed after env
 //order - printed as they are created!
