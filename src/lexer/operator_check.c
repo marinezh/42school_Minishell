ed059d074ca490cc145a337697c72be37f2a5318
Marina Zhivotova <mazhivotova@gmail.com>
2025-05-12 20:21:13 +0300

    deleted extra check for || as we do not do bonus

--- src/lexer/operator_check.c
+++ src/lexer/operator_check.c
@@ -1,5 +1,23 @@
 #include "minishell.h"
 
+int	is_single_op(char *input, int i)
+{
+	if (input[i] == '|' || input[i] == '&' || input[i] == '<'
+		|| input[i] == '>')
+		return (1);
+	return (-1);
+}
+
+int	is_dbl_op(char *input, int i)
+{
+	if (!input[i + 1])
+		return (-1);
+	if ((input[i] == '>' && input[i + 1] == '>') 
+			|| (input[i] == '<' && input[i + 1] == '<'))
+		return (1);
+	return (-1);
+}
+
 void	process_one(char *input, char *output, int *i, int *j)
 {
 	output[(*j)++] = ' ';
