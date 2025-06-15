2702395b5b91081207ff9b2e81e8201fc698b79b
marinezh <mazhivotova@gmail.com>
2025-06-15 21:41:29 +0300

    add final quotes removing

--- src/parser/clean_quotes.c
+++ src/parser/clean_quotes.c
@@ -1,55 +1,39 @@
 #include "minishell.h"
 
-// int is_closed_quote(t_token *token)
-// {
-// 	int i = 0;
-// 	char quotes = '\'';
-// 	if (token->value[i] == quotes)
-// 	{
-// 		while (token->value[i] && token->value[i] != quotes)
-// 			i++;
 
-// 	}
-// }
-// void clean_quotes(t_token *token)
-// {
-// 	int i = 0;
-// 	char quotes = '\'';
-// 	while (token->value[i])
-// 	{
-// 		if (token->value[i] == quotes)
-// 			token->value[i] = '\0';
-// 	}
-	
-// }
-void remove_outer_quotes(t_token *token)
+void remove_outer_quotes(t_token *tokens)
 {
-    if (!token || !token->value)
-        return;
-
-    int i = 0, j = 0;
-    char *input = token->value;
-    char *result = malloc(ft_strlen(input) + 1);
-    if (!result)
-        return; // handle malloc failure
-
-    while (input[i])
-    {
-        if (input[i] == '\'' || input[i] == '"')
-        {
-            char quote = input[i++];
-            while (input[i] && input[i] != quote)
-                result[j++] = input[i++];
-            if (input[i] == quote)
-                i++; // skip closing quote
-        }
-        else
-        {
-            result[j++] = input[i++];
-        }
-    }
-    if (token->value)
-        free(token->value);
-
-    token->value = result;
+	t_token *current = tokens;
+	
+	while (current)
+	{
+		if (!current->value)
+		{
+			current = current->next;
+			continue;
+		}
+		int i = 0, j = 0;
+		char *input = current->value;
+		char *result = malloc(ft_strlen(input) + 1);
+		if (!result)
+			return;
+	
+		while (input[i])
+		{
+			if (input[i] == '\'' || input[i] == '"')
+			{
+				char quote = input[i++];
+				while (input[i] && input[i] != quote)
+					result[j++] = input[i++];
+				if (input[i] == quote)
+					i++; // skip closing quote
+			}
+			else
+				result[j++] = input[i++];
+		}
+		result[j] = '\0';
+		free(current->value);
+		current->value = result;
+		current = current->next;
+	}
 }
