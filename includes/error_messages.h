11966d36fac04045b45098fcf34641273840cb2e
Marina Zhivotova <mazhivotova@gmail.com>
2025-06-24 14:28:59 +0300

    add new tester

--- includes/error_messages.h
+++ includes/error_messages.h
@@ -5,7 +5,7 @@
 # define MSG_NO_OPTS_ARGS ": options or arguments not supported\n"
 # define MSG_INVALID_NAME ": variable name is invalid\n"
 # define MSG_TOO_MANY_ARGS ": too many arguments\n"
-# define MSG_NO_PERM ": permission denied\n"
+# define MSG_NO_PERM ": Permission denied\n"
 # define MSG_CMD_NOT_FOUND ": command not found\n"
 # define MSG_NO_FILE ": No such file or directory\n"
 # define ERR_QUOTES "Quotes are not closed\n"
