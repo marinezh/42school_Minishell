eed4ac3df05dffad4745938430f110a0d66d8717
Marina Zhivotova <mazhivotova@gmail.com>
2025-07-01 19:54:49 +0300

    in process of norm for parser part

--- src/parser/error_check.c
+++ src/parser/error_check.c
@@ -1,103 +1,58 @@
 #include "minishell.h"
-int check_unclosed_quotes(char *str)
-{
-	int i = 0;
-	char quote = 0;
 
-	while (str[i])
+int is_redirect_type(t_token *token)
+{
+	if (token->type == REDIR_OUT || token->type == REDIR_APPEND
+		|| token->type == REDIR_IN || token->type == HEREDOC)
 	{
-		if ((str[i] == '\'' || str[i] == '"'))
-		{
-			if (!quote)
-				quote = str[i];
-			else if (str[i] == quote)
-				quote = 0;
-		}
-		i++;
+		return 1;
 	}
-	return (quote != 0);
+	else 
+		return 0;
 }
 
 int error_check(t_token *token, t_data *data)
 {
-	// Check if command starts with a pipe
 	if (token && token->type == PIPE)
 	{
 		handle_error_arg(data, "", ERR_PARSING_PIPE, 2);
 		data->status = 2;
-		//printf("ONE PIPE\n");
 		return 1;
 	}
 	while(token)
 	{
-		// if ((token->type == REDIR_OUT || token->type == REDIR_APPEND || token->type == REDIR_IN) && (token->next))  
-		// {
-
-		// }
-		
-		
 		if(token->type == PIPE && (!token->next || token->next->type == PIPE))
 		{
 			handle_error_arg(data, "", ERR_PARSING_PIPE, 2);
-			//data->status = 2;
-			//printf("TW0 PIPES\n");
 			return (1);
 		}
 		// case < > << >> (newline error)
-		if ((token->type == REDIR_APPEND || token->type == REDIR_IN || 
-			token->type == REDIR_OUT || token->type == HEREDOC) && !token->next)
+		if (is_redirect_type(token) && !token->next)
 		{
 			handle_error_arg(data, "", ERR_PARSING_RED, 2);
-			
-			//data->status = 2;
-			//printf("FOUR\n");
-		
 			return (1);
 		}
-	
-        // Handle the special case of REDIR_APPEND followed by REDIR_OUT >>> case 
-        else if ((token->type == REDIR_OUT || token->type == REDIR_APPEND || token->type == REDIR_IN || token->type == HEREDOC)
-				 && token->next && (token->next->type == REDIR_OUT))
-        {
-            handle_error_arg(data, "", ERR_PARSING_OUT, 2);
-			//printf(" syntax error near unexpected token `>'\n");
-			//printf(">>>\n");
-            return 1;
-        }
-		// else if((token->type == REDIR_APPEND || token->type == REDIR_IN || 
-		// 	token->type == REDIR_OUT || token->type == HEREDOC) &&
-		// 	(!token->next || token->next->type == REDIR_APPEND ||
-		// 	token->next->type == REDIR_IN || token->next->type == REDIR_OUT ||
-		// 	token->next->type == HEREDOC))
-		// {
-		// 	handle_error_arg(data, "", ERR_PARSING_RED2, 2);
-			
-		// 	//data->status = 2;
-		// 	//printf("FOUR\n");
-		// 	return (1);
-		// }
-		else if ((token->type == REDIR_OUT || token->type == REDIR_APPEND || token->type == REDIR_IN || token->type == HEREDOC) && token->next && (token->next->type == REDIR_APPEND))
+		// Handle the special case of REDIR_APPEND followed by REDIR_OUT >>> case 
+		else if (is_redirect_type(token) && token->next && (token->next->type == REDIR_OUT))
+		{
+			handle_error_arg(data, "", ERR_PARSING_OUT, 2);
+			return 1;
+		}
+		else if (is_redirect_type(token)
+			&& token->next && (token->next->type == REDIR_APPEND))
 		{
 			handle_error_arg(data, "", ERR_PARSING_APP, 2);
-			//printf(" syntax error near unexpected token `>'\n");
-		// 	//data->status = 2;
-		// 	//printf("FOUR\n");
 			return (1);
 		}
-		else if ((token->type == REDIR_OUT || token->type == REDIR_APPEND || token->type == REDIR_IN || token->type == HEREDOC) && token->next && (token->next->type == REDIR_IN))
+		else if (is_redirect_type(token)
+			&& token->next && (token->next->type == REDIR_IN))
 		{
 			handle_error_arg(data, "", ERR_PARSING_IN, 2);
-			//printf(" syntax error near unexpected token `>>'\n");
-		// 	//data->status = 2;
-		// 	//printf("FOUR\n");
 			return (1);
 		}
-		else if ((token->type == REDIR_OUT || token->type == REDIR_APPEND || token->type == REDIR_IN || token->type == HEREDOC) && token->next && (token->next->type == HEREDOC))
+		else if (is_redirect_type(token) && token->next && (token->next->type == HEREDOC))
 		{
 			handle_error_arg(data, "", ERR_PARSING_HER, 2);
-			//printf(" syntax error near unexpected token `>>'\n");
-		// 	//data->status = 2;
-		// 	//printf("FOUR\n");
 			return (1);
 		}
 		else if((token->type == REDIR_APPEND || token->type == REDIR_IN || 
@@ -105,9 +60,6 @@ int error_check(t_token *token, t_data *data)
 			(!token->next || token->next->type == PIPE ))
 		{
 			handle_error_arg(data, "", ERR_PARSING_PIPE, 2);
-		
-			//data->status = 2;
-			//printf("FIVE\n");
 			return (1);
 		}
 
